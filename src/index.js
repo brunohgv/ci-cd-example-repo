44863418c86daaa56fc3d4be8317171c3d431de7
Bruno Henrique Gusmao Vasconcelos-BRUNOV <brunov@motorola.com>
2020-11-20 09:41:26 -0300

    Add jsdoc to subtract function

--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
-/**
- * Sums two numbers
- *
- * @param {number} a first number
- * @param {number} b secund number
- */
-function sum(a, b) {
-    return a + b;
-};
+const app = require('./app')
 
-function subtract(a, b) {
-    return a - b;
-};
-
-function multiply(a, b) {
-    return a * b;
-};
-
-function divide(a, b) {
-    return a / b;
-};
-
-module.exports = {
-    sum,
-    subtract,
-    multiply,
-    divide
-};
+app.listen(3333, () => {
+    console.log('Running on port 3333')
+})
